55bde3b490766510446cdb87a0c6e0b3e185c889
developer-glareus <developer.glareus@gmail.com>
2022-11-27 14:29:50 +0530

    loops

--- oops/index.js
+++ oops/index.js
@@ -1,36 +1,196 @@
-class Person {
-  constructor(name, age) {
-    this.name = name; // properities
-    this.age = age;
-  }
+// class Person {
+//   constructor(name, age) {
+//     this.name = name; // properities
+//     this.age = age;
+//   }
 
-  detail() {
-    // method
-    console.log(`my name is ${this.name} and my age is ${this.age}`);
-  }
-}
+//   detail() {
+//     // method
+//     console.log(`my name is ${this.name} and my age is ${this.age}`);
+//   }
+// }
 
-const jerin = new Person("jerin", 27); // instince
+// const jerin = new Person("jerin", 27); // instince
 
-// console.log(jerin instanceof Person);
+// // console.log(jerin instanceof Person);
 
-// const ligi = new Person("ligi", 25);
+// // const ligi = new Person("ligi", 25);
 
-// jerin.detail();
-// ligi.detail();
+// // jerin.detail();
+// // ligi.detail();
 
-class Student extends Person {
-  constructor(school, name, age) {
-    super(name, age);
-    this.school = school;
-  }
+// class Student extends Person {
+//   constructor(school, name, age) {
+//     super(name, age);
+//     this.school = school;
+//   }
 
-  mySchool() {
-    console.log("my school name is", this.school);
-  }
-}
+//   mySchool() {
+//     console.log("my school name is", this.school);
+//   }
+// }
 
-const studentLigi = new Student("excel", "ligi", 23);
+// const studentLigi = new Student("excel", "ligi", 23);
 
-studentLigi.mySchool();
-studentLigi.detail();
+// studentLigi.mySchool();
+// studentLigi.detail();
+
+// for loop
+// for in loop
+// for of loop
+
+// while loop;
+// do while loop;
+
+// array methods
+// map
+// filter
+// find
+// reduce
+// every
+// some
+
+// for loop;
+
+// for (let i = 0; i < 10; i++) {
+//   console.log(i);
+// }
+
+let name = "jerin";
+let list = [1, 4, 3, 4, "jerin", 6, 9, "ligi", "rejo", 8];
+
+// console.log(list.length);
+// console.log(name.length);
+
+// console.log(name[2]);
+// console.log(list[3]);
+
+// for (let i = 0; i < name.length; i++) {
+//   console.log(name[i]);
+// }
+
+// for (let i = 0; i < list.length; i++) {
+//   console.log(list[i]);
+// }
+
+// for (let i in name) {
+//   // console.log(i);
+//   console.log(name[i]);
+// }
+
+// let tempStr = [];
+// let temNumber = [];
+
+// let x = "5";
+
+// console.log(typeof x);
+
+// for (let i in list) {
+//   if (typeof list[i] === "string") {
+//     tempStr.push(list[i]);
+//     continue;
+//   }
+//   temNumber.push(list[i]);
+// }
+
+// console.log(tempStr);
+// console.log(temNumber);
+
+// for (let val  of list) {
+//   console.log(val)
+// }
+
+// break     --> exit
+// continue  --> skip
+
+// for (let i = 1; i <= 10; i++) {
+//   if (i === 5) {
+//     break;
+//   }
+//   console.log(i);
+// }
+//
+
+// for (let i = 1; i <= 10; i++) {
+//   if (i === 5) {
+//     continue;
+//   }
+//   console.log(i);
+// }
+
+// let stringArray = ["jerin", "happy", "ligi", "rejo"];
+
+// function upperCaseByPositon(arr, indx) {
+//   let str = arr[indx];
+//   console.log(str.toUpperCase());
+// }
+
+// upperCaseByPositon(stringArray, 1);
+
+// object looping --> important
+
+// Object.values()
+// Object.keys()
+// Object.entries()
+
+const obj = { name: "jerin", age: 25, city: "ngl" };
+
+// let val = Object.values(obj);
+// console.log(val);
+
+// let keys = Object.keys(obj);
+// console.log(keys);
+
+// let keyAndVal = Object.entries(obj);
+// console.log(keyAndVal);
+
+// for (let key of Object.keys(obj)) {
+//   console.log(key);
+// }
+
+// for (let key of Object.values(obj)) {
+//   console.log(key);
+// }
+
+// for (let [key, value] of Object.entries(obj)) {
+//   console.log(key, "--------------------", value);
+// }
+
+// array methods
+// map
+// filter
+// forEach
+// find
+// reduce
+// every
+// some
+
+let myArr = [5, 7, 2, 4, 6, 3, 8, 5];
+
+// myArr.forEach((val, idx) => {
+//   console.log(val, "--------->", idx);
+// });
+
+// let findValue = myArr.find((val, idx) => {
+//   console.log(val % 2 === 0);
+//   return val % 2 === 0;
+// });
+// console.log(findValue);
+
+// let filterVals = myArr.filter((val, indx) => {
+//   return val % 2 === 0;
+// });
+
+// console.log(filterVals);
+
+// const mapVals = myArr.map((val, idx) => {
+//   return val * 2;
+// });
+
+// console.log(mapVals);
+
+// const reduceVal = myArr.reduce((val, acc) => {
+//   return val + acc;
+// }, 0);
+
+// console.log(reduceVal);
---
+++ test/test.js
@@ -0,0 +1,23 @@
+// let obj = {
+//   nameList: [
+//     { name: "jeirn", age: 22 },
+//     { name: "ligi", age: 25 },
+//     { name: "rejolin", age: 24 },
+//   ],
+//   metadata: {
+//     detail: {
+//       subDetail: {
+//         list: [44, 77],
+//       },
+//     },
+//   },
+// };
+
+let React = {
+  Component: "somefpwi f",
+};
+
+const comp = React.Component;
+const { Component } = React;
+
+console.log(Component);
